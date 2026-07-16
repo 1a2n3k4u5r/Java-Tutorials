25a196f7f9a65482d134c9376eafbee09094f491
1a2n3k4u5r <ankur1236876@gmail.com>
2026-07-16 21:20:29 +0530

    Add Main and Pattern classes with basic output functionality

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args){
+         //output
+         System.out.println("Hello World ");
+         System.out.print("Hello World in java\n");
+         System.out.println("Hello World in hindi ");
+         System.out.print("Hello World with java\n from Ankur Yadav");
+    
+    }
+}
+
+// Java ma jab tak app semicolon nhi lagata tab tak line chalti rehti hai.
+// where print is the function in java
+// where System is the class in java
---
+++ Pattern.java
@@ -0,0 +1,10 @@
+public class Pattern {
+    public static void main(String[] args){
+        // output 
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
+    
