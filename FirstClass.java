7e1ffd95806588d306adeb8406f63a20cd18aa15
1a2n3k4u5r <ankur1236876@gmail.com>
2026-07-16 19:01:15 +0530

    java syntax and about java

--- FirstClass.java
+++ FirstClass.java
@@ -3,3 +3,21 @@ class FirstClass {
        System.out.println("Hello World");
     }
 }
+
+// JDK and Development Tools are the parent of JRE and JVM + Libraries or the JRE is the parent of JVM.
+
+// Java is the portable language.
+
+// How is code running?
+// 1) Compilation : source code .java + compiler by Jdk + Byte Code .class
+// 2) Execution : Byte Code + JVM + native code.
+
+
+// components of code are: 
+// 1) Function / Methods: jo bhi code huma java ka andar likhana hota ha vo hum function ka andar likhta hai. Ex: void main(){}
+
+//  2) Class : in ma hum function ko likhta hai. Ex: class Main {
+//     void main(){
+
+//     }
+//  }
