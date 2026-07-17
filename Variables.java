c75dc5995b7e15534e77a020f1c2052e86ca38b6
1a2n3k4u5r <ankur1236876@gmail.com>
2026-07-17 07:39:22 +0530

    Add Variables and DataTypes classes with initial variable declarations and comments

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+   public static void main(String[]args){
+    //variables
+    String name = "ankur yadav";
+    int age = 21;
+    double price = 25.25;
+    int a = 25;
+    int b = 10;
+
+    b = 20;
+    name = "ironman";
+   } 
+} 
---
+++ datatypes.java
@@ -0,0 +1,5 @@
+// Java is a typed language is language ma koi bhi variable batna se phala ye batna padta ha ki us variable ka type kya hai
+
+// Data Types are two types -
+// 1) Primitive data types: byte,short,char,boolean,int,long,float,double.
+// 2) Non-Primitive data type: String,Array,Class,Object,Interface.
