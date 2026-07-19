cffccfc1477f61bf8f5e5f93f4178fb1a440fbd8
1a2n3k4u5r <ankur1236876@gmail.com>
2026-07-19 10:36:50 +0530

    Add Space and Time Complexity examples with time and space complexity explanations

---
+++ Space and Time Complexity.java
@@ -0,0 +1,89 @@
+// Time complexity
+// Relation between Input Size and Running Time(operations)
+
+// 1) public static void main(String args[]){
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for(int i=0; i<n; i++){
+ // System.out.println("hello");
+// }
+// }
+
+// output = time complexity - n times
+
+
+// 2) Base Case - omega(1)
+// Average Case - 0(n+1/2)
+// Worst Case - 0(n)
+
+
+// // 3)
+//  public static void main(String args[]){
+//  Scanner sc = new Scanner(System.in);
+//  int n = sc.nextInt();
+
+//  for(int i=0; i<n; i++){
+//     for(int j=0; j<n; j++){
+//         System.out.println("hello");
+//     }
+//  }
+// }  
+
+// output = 0(n^2)
+
+
+
+// 4)
+
+// public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int m = sc.nextInt();
+
+//     for(int i=0;i<n;i++){
+//         for(int j=0; j<m; j++){
+//             System.out.println("hello");
+//         }
+//     }
+// }
+
+// time complexity = 0(n*m)
+
+
+
+// 6)
+// public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int m = sc.nextInt();
+
+//     for(int i=0; i<n; i++){
+//         System.out.println("hello");
+//     }
+//     for(int j=0;j<m;j++){
+//         System.out.println("hello");
+//     }
+// }
+
+// output = O(n)
+
+
+
+// 7)
+// Compare: n  n^2   n^3
+
+
+
+// 1) Space Complexity
+
+// public static void main(String args[]){
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+
+//    for(int i=0;i<n;i++){
+//      System.out.println("hello");
+//    }
+// }
+
+
+
