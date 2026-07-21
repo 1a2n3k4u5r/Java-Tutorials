07549dc3305e50ae71348cebc7f70caffbbac9e5
1a2n3k4u5r <ankur1236876@gmail.com>
2026-07-21 08:34:52 +0530

    Add Operators class with detailed comments on various operator types and their usage

---
+++ Operators.java
@@ -0,0 +1,65 @@
+// Operators- Symbols that tell compiler to perform some operation.
+//  a + b where a,b are the opeartors and + sign is a operand.
+
+
+//  Types of opearators are :
+// Aritmetic Operators 
+// Binary are +,-,*,/,% . here  these  (%)modulo which is used to find the remainders.
+// Uniary are ++, -- . these are increment and decrement.
+
+// Pre Increment = ++a here change value than use value.
+// int a = 10;
+// int b = 0;
+// b = a++;
+// System.out.println(a);
+// System.out.println(b);
+// output a = 11, b = 10.
+
+// Post Increment = a++  here use value than change value.
+// int a = 10;
+// int b = 0;
+// b = ++a;
+// System.out.println(a);
+// System.out.println(b);
+// output a = 11,  b = 11
+
+
+// Pre decrement = --a here value change than value assign.
+
+// int a = 10;
+// int b = 0;
+// b = --a;
+// System.out.println(a);
+//System.out.println(b);
+// output are a = 9, b = 9;
+
+// Post Decrement = a-- here value assign than value change
+// int a = 10;
+// int b = 0;
+// b = a--;
+// System.out.println(a);
+// System.out.println(b);
+// output a = 10, b = 9
+
+
+// RELATIONAL OPERATORS
+// OPERATORS ARE == equal to, != not equal to, > greater, <, >=, <=,
+
+
+// LOGICAL OPERATORS
+// &&(Logical AND)
+// ||(Logical OR)
+// !(LOgical NOT)
+
+// Binary Number System - Base2
+// 4 to Binary
+// 101 to Decimal
+// Octal- Base 8, Hexdecimal- Base 16
+
+
+// Bitwise Operators 
+// & (Binary AND), | (Binary OR), ^(Binary XOR), ~(Binary One's Complement), << (Binary Left Shift), >>(Binary Right Shift)
+
+
+// Assignment Operators 
+// =, +=, -=, *=, /=
